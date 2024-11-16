282d05282d7a89ff1eef2c37abc5f9c54771d4eb
01Temur <tjurabekov@iuj.ac.jp>
2024-11-16 15:52:51 +0900

    Update stockscope_app.py

--- stockscope_app.py
+++ stockscope_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from yahoo_fin import stock_info as si
 import pandas as pd
-import requests
+import requests-html
 
 # Function to fetch stock data for each category
 def fetch_stock_data(category):
